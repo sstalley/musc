5540a47dcd613a8a30cf6ce6a25d60937773385a
Sean O. Stalley <sstalley@pdx.edu>
2023-04-13 15:27:38 -0700

    Better py1 checker
    Now I need to read the results...

--- py1_tester.py
+++ py1_tester.py
@@ -6,13 +6,16 @@ from runner_grader import MAX_RUNTIME, PY1_MAX_SCORE, test_dir
 from py1_roulette import e12rs
 from pathlib import Path
 
-inputzz = ["10.0", "10.0", "", "", ""]
 #various semi-common voltages
 voltzz = [ "1.5", "3.3", "3.7", "5.0", "7.2", "12", "24", "120"]
 
 
 #faking suff hopefully?
-def input():
+def __input(prompt=None):
+    print("__input() HIT!")
+    if prompt is not None:
+        print(f"prompt:{prompt}")
+
     return "10.0"
 
 def ol(v,r):
@@ -39,25 +42,43 @@ def score_fn(fn_name, ctrl_name, fn_valuez, ctrl_valuez):
 
     # can I call it?
     try:
-        exec(f"{fn_name}({', '.join(fn_valuez[0])})\n")
+        call_str = f"{fn_name}({', '.join(fn_valuez[0])})"
+        print(f"calling {call_str}")
+        exec(call_str)
+
     except NameError:
         print(f"Could not find function {fn_name}")
         return 0
 
     # does it return valid values?
     for fn_i in fn_valuez:
-        exec(f"x = {fn_name}({', '.join(fn_i)})\nprint(x)\n")
-        print(x)
+        call_str = f"x = {fn_name}({', '.join(fn_i)})"
+        loc = {}
         try:
+            exec(call_str, globals(), loc)
+            x = loc['x']
             int(x)
         except ValueError:
             print(f"Could not understand output {x}")
             return 1
+        except Exception as e:
+            print(e)
+            print(f"Not gonna try any harder...")
+            return 1
 
     for fn_i, ctrl_i in zip(fn_valuez, ctrl_valuez):
-        exec(f"x = {fn_name}({', '.join(fn_i)})\n")
-        exec(f"y = {ctrl_name}({', '.join(ctrl_i)})\n")
-        if not within_tol(x, y):
+        loc = {}
+        try:
+            exec(f"x = {fn_name}({', '.join(fn_i)})", globals(), loc)
+            exec(f"y = {ctrl_name}({', '.join(ctrl_i)})\n", globals(), loc)
+            x = loc['x']
+            y = loc['y']
+        except Exception as e:
+            print(e)
+            print(f"Not gonna try any harder...")
+            return 2
+
+        if not within_tol([x], [y]):
             print(f"{x} and {y} too different")
             return 2
 
@@ -74,6 +95,7 @@ with open(fileName, mode='r') as file: # b is important -> binary
     source_code = file.read()
 
 
+
 # GLOBAL CHECKS (does it call input twice?)
 inputs = len(re.findall("input\(", source_code))
 print(f"Inputs counted: {inputs}")
@@ -84,36 +106,40 @@ if inputs == 2:
     score = 2
 
 
+# Semi-Hack: go to my input:
+print(f"old source code:\n{source_code}")
+source_code = re.sub("input\(", "__input(", source_code)
+print(f"new source code:\n{source_code}")
+
+
 fns = ["calc_total_r", "ohms_law", "r3_voltage"]
 ctrls = ["r_calc", "ol", "vdiv"]
 
 
-for fn in fns:
-    try:
-        #TODO: un-hard-code
-        exec(f"from test_dir.{Path(fileName).stem} import {fn}")
-    except Exception as e:
-        print(e)
-        print("something bad happend when importing functions")
+try:
+    exec(source_code)
+except Exception as e:
+    print(e)
+    print("something bad happend when importing functions")
 
 
 #TODO: actually lookup correct resistance values
 # For now just use fixed values for sstalley
-r1 = "100"
-r2 = "120"
+__r1 = "100"
+__r2 = "120"
 
 #Sweep resistor values and check
 fn_values_1   = [[str(r)] for r in e12rs]
-ctrl_values_1 = [[r1, r2, str(r)] for r in e12rs]
+ctrl_values_1 = [[__r1, __r2, str(r)] for r in e12rs]
 score = score + score_fn(fns[0], ctrls[0], fn_values_1, ctrl_values_1)
 
 #fn and ctrl are the same here
-ohm_values = [[v, r1] for v in voltzz]
+ohm_values = [[v, __r1] for v in voltzz]
 score = score + score_fn(fns[1], ctrls[1], ohm_values, ohm_values)
 
 #sweep voltages and check
-fn_values_3   = [[r1, v] for v in voltzz]
-ctrl_values_3 = [[r1, r1, v] for v in voltzz]
+fn_values_3   = [[__r1, v] for v in voltzz]
+ctrl_values_3 = [[__r2, __r1, v] for v in voltzz]
 score = score + score_fn(fns[2], ctrls[2], fn_values_3, ctrl_values_3)
 
 print(f"MUSC TOTAL {score}")
